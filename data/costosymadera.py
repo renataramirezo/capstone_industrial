a03ab27c0a15ba758e209d791bbd9f7530a0d5a5
MSOFIA WAHL <mswahl@uc.cl>
2025-04-29 12:36:07 -0400

    credaor de diccionario nodos

---
+++ data/costosymadera.py
@@ -0,0 +1,26 @@
+import pandas as pd
+'''este archivo es para poner los costos de instalación de faena 
+y la cantidad de madera por rodal'''
+excel = pd.read_excel("data\costos y madera nodos.xlsx")
+
+#['idnodo', 'eje vertical', 'eje horizontal', 'tipo de feane', 
+# 'costo inst', 'cant madera']
+# Seleccionar solo las columnas necesarias
+excel2 = excel.rename(columns={
+    'tipo de feane': 'K',
+    'costo inst': 'cf',
+    'cant madera': "v"
+})
+
+columnas_deseadas = ['idnodo', 'K', 'cf', 'v']
+df = excel2[columnas_deseadas]
+df = df.dropna(subset=['idnodo'])
+# Establecer 'idnodo' como índice
+df.set_index('idnodo', inplace=True)
+
+# Convertir a diccionario
+dic_nodos = df.to_dict(orient="index")
+
+
+
+
