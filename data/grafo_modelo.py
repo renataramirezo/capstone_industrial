6c72181490c9b7b1c32e63ac8192f35b03e9a3ab
MSOFIA WAHL <mswahl@uc.cl>
2025-05-11 23:19:36 -0400

    ultima actualizacion a grafos_modelo

--- data/grafo_modelo.py
+++ data/grafo_modelo.py
@@ -6,6 +6,8 @@ import matplotlib.colors as mcolors
 import datos as dt
 import os
 plt.show()
+edge_colors = [
+    "white" for u, v in gf.G.edges()]
 
 with open('resultados_modelo.pkl', 'rb') as f:
     resultados = pickle.load(f)
@@ -17,10 +19,10 @@ T=[1,2,3,4,5,6,13,14,15,16,17,18]
 T1=[1,2,3,4,5,6]
 T2=[13,14,15,16,17,18]
 # Por ejemplo, iterar para imprimir solo las aristas activadas en t=0:
-node_colors_gris = ["grey"] * len(gf.G.nodes())
+node_colors_gris = ["white"] * len(gf.G.nodes())
 nodes_list = list(gf.G.nodes())
 node_colors_t_1 = node_colors_gris.copy()
-'''for t in T1:
+for t in T1:
     # --- Colorear nodos
     for i in gf.G.nodes():
         es_faena = any(f.get((i, k, t), 0) > 0.5 for k in dt.K)
@@ -41,8 +43,8 @@ gf.nx.draw(
     gf.G, gf.pos,
     with_labels=True,
     node_color=node_colors_t_1,
-    edge_color=gf.edge_colors,
-    edgecolors= gf.nodo_bordes_faen,
+    edge_color=edge_colors,
+    #edgecolors= gf.nodo_bordes_faen,
     linewidths= 1.5,
     node_size=150,
     font_size=5
@@ -57,7 +59,7 @@ gf.nx.draw_networkx_edges(
 )
 
 # --- (Opcional) Dibujar flechas de faena a hectáreas cosechadas
-for (i, j, k_, t_), val in x.items():
+'''for (i, j, k_, t_), val in x.items():
     if t_ == t and val > 0.5 and i in gf.G.nodes() and j in gf.G.nodes():
         gf.nx.draw_networkx_edges(
             gf.G, gf.pos,
@@ -67,7 +69,7 @@ for (i, j, k_, t_), val in x.items():
             arrowstyle='->',
             arrowsize=12,
             connectionstyle="arc3,rad=0.2"
-        )
+        )'''
 
 plt.title(f"Grafo de faenas y caminos presentes en temporada 1")
 plt.axis('off')
@@ -99,8 +101,8 @@ gf.nx.draw(
     gf.G, gf.pos,
     with_labels=True,
     node_color=node_colors_t_1,
-    edge_color=gf.edge_colors,
-    edgecolors= gf.nodo_bordes_faen,
+    edge_color=edge_colors,
+    #edgecolors= gf.nodo_bordes_faen,
     linewidths= 1.5,
     node_size=150,
     font_size=5
@@ -114,7 +116,7 @@ gf.nx.draw_networkx_edges(
     width=3.5
 )
 
-# --- (Opcional) Dibujar flechas de faena a hectáreas cosechadas
+'''# --- (Opcional) Dibujar flechas de faena a hectáreas cosechadas
 for (i, j, k_, t_), val in x.items():
     if t_ == t and val > 0.5 and i in gf.G.nodes() and j in gf.G.nodes():
         gf.nx.draw_networkx_edges(
@@ -125,14 +127,14 @@ for (i, j, k_, t_), val in x.items():
             arrowstyle='->',
             arrowsize=12,
             connectionstyle="arc3,rad=0.2"
-        )
+        )'''
 
 plt.title(f"Grafo de faenas y caminos presentes en temporada 2")
 plt.axis('off')
 output_path = os.path.join("data","grafos_faenas_modelo", f"grafo_resultado_modelo_en Temp 2.png")
 plt.savefig(output_path, format="png", dpi=300, bbox_inches="tight")
 plt.close() 
-plt.show()'''
+plt.show()
 for t in T:
     node_colors_t = node_colors_gris.copy()
     #plt.figure(figsize=(6, 6))
@@ -161,8 +163,8 @@ for t in T:
         gf.G, gf.pos,
         with_labels=True,
         node_color=node_colors_t,
-        edge_color=gf.edge_colors,
-        edgecolors= gf.nodo_bordes_faen,
+        edge_color=edge_colors,
+        #edgecolors= gf.nodo_bordes_faen,
         linewidths= 1.5,
         node_size=150,
         font_size=5
