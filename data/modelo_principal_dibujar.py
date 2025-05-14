9fcedaaab0a107160dfbe35891f063738f74b363
MSOFIA WAHL <mswahl@uc.cl>
2025-05-13 23:35:02 -0400

    funciones para dibujar el modelo

---
+++ data/modelo_principal_dibujar.py
@@ -0,0 +1,14 @@
+from grafo_modelo import dibujar_grafo_por_temporada
+from grafo_modelo import dibujar_grafo_por_cada_t
+'''esto dibujará el archivo pkl y guardará cada imágen 
+en la carpeta de salida'''
+
+dibujar_grafo_por_temporada(
+    archivo_pkl="resultados_modelo_principal.pkl",
+    carpeta_salida="grafos_faenas_modelo_ppl",
+)
+
+dibujar_grafo_por_cada_t(
+    archivo_pkl="resultados_modelo_principal.pkl",
+    carpeta_salida="grafos_faenas_modelo_ppl",
+)
